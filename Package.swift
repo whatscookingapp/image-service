02772670684dc61ff534e33c682a381dca7181ba
Jimmy Arts <arts.jimmy@gmail.com>
2019-08-06 16:38:05 +0200

    Implement scope

--- Package.swift
+++ Package.swift
@@ -9,9 +9,10 @@ let package = Package(
         .package(url: "https://github.com/vapor-community/vapor-ext.git", from: "0.3.0"),
         .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0-RC3.2"),
         .package(url: "https://gitlab.com/jimmya92/oauthvalidator.git", .branch("master")),
+        .package(url: "https://gitlab.com/food-sharing/scopes.git", .branch("master")),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL", "S3", "ServiceExt", "OAuthValidator"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL", "S3", "ServiceExt", "OAuthValidator", "scopes"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/Controllers/ImageController.swift
+++ Sources/App/Controllers/ImageController.swift
@@ -1,17 +1,28 @@
 import Vapor
+import OAuthValidator
+import scopes
 import S3
 
 struct ImageController: RouteCollection {
     
     func boot(router: Router) throws {
         let imagesRouter = router.grouped("images")
-        
-        imagesRouter.post(ImageRequest.self, at: "", use: generateImage)
         imagesRouter.get(UUID.parameter, use: getImage)
+        
+        let authenticatedRouter = imagesRouter.grouped(UserIdMiddleware(), ScopeValidationMiddleware(scope: .image))
+        authenticatedRouter.post(ImageRequest.self, at: "", use: generateImage)
     }
 }
 
 private extension ImageController {
+    
+    func getImage(_ req: Request) throws -> Future<GetImageResponse> {
+        let id = try req.parameters.next(UUID.self)
+        let repository = try req.make(ImageRepository.self)
+        return repository.find(id: id, on: req).unwrap(or: Abort(.notFound)).map(to: GetImageResponse.self) { image in
+            return GetImageResponse(id: try image.requireID())
+        }
+    }
     
     func generateImage(_ req: Request, request: ImageRequest) throws -> Future<CreateImageResponse> {
         try request.validate()
@@ -23,7 +34,7 @@ private extension ImageController {
         let s3 = try req.makeS3Client()
         let upload = File.Upload(data: Data(), destination: fileName, access: .publicRead)
         let url = try s3.url(fileInfo: upload, on: req)
-        let headers: [String: String] = ["x-amz-acl": AccessControlList.publicRead.rawValue, "Content-Length": "\(request.bytes)"]
+        let headers: [String: String] = ["Content-Length": "\(request.bytes)"]
         guard let presignedURL = try s3Signer.presignedURL(for: .PUT, url: url, expiration: .fifteenMinutes, region: nil, headers: headers) else {
             throw Abort(.internalServerError)
         }
@@ -31,14 +42,6 @@ private extension ImageController {
         let repository = try req.make(ImageRepository.self)
         return repository.save(image: image, on: req).map { image in
             return CreateImageResponse(id: try image.requireID(), url: presignedURL, headers: headers)
-        }
-    }
-    
-    func getImage(_ req: Request) throws -> Future<GetImageResponse> {
-        let id = try req.parameters.next(UUID.self)
-        let repository = try req.make(ImageRepository.self)
-        return repository.find(id: id, on: req).unwrap(or: Abort(.notFound)).map(to: GetImageResponse.self) { image in
-            return GetImageResponse(id: try image.requireID())
         }
     }
 }
